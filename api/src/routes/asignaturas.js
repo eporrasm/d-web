5860964ea5a25b6c042d6565462a6aa3221cf6e3
eporrasm <vals4oo@gmail.com>
2023-04-21 11:33:27 -0500

    commit api

--- api/src/routes/asignaturas.js
+++ api/src/routes/asignaturas.js
@@ -1,65 +1,39 @@
 const { Router } = require('express');
-const router = Router();
 const asignaturas = require('../sample.json');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const { spawn } = require('child_process');
 const _ = require('underscore');
 
-router.get('/', (req, res) => {
-    res.status(200).json(asignaturas["asignaturas"]);
-});
+const router = Router();
 
 router.post('/', (req, res) => {
-    const {nota, creditos, nombre, codigo, 
-        tipologia, semestre, prerrequisitos, 
-        estado, afecta_promedio} = req.body;
-    if (creditos && nombre && codigo && 
-        tipologia && semestre && 
-        estado && afecta_promedio) {
-        const newAsignatura = {...req.body};
-        asignaturas["asignaturas"].push(newAsignatura);
-        res.status(200).json(asignaturas["asignaturas"]);
-    } else{
-        res.status(500).json({error: 'There was an error.'});
-    }
-});
+    const input = req.body;
 
-router.put('/:semestre/:codigo', (req, res) => {
-    const semestre = parseInt(req.params.semestre);
-    const codigo = req.params.codigo;
-    const {nota, creditos, nombre,
-        tipologia, prerrequisitos,
-        estado, afecta_promedio} = req.body;
-    if (creditos && nombre && codigo && 
-        tipologia && semestre && 
-        estado && afecta_promedio){
-    _.each(asignaturas["asignaturas"], (asignatura, i) => {
-        if (asignatura.semestre === semestre && 
-            asignatura.codigo === codigo){
-            asignatura.nota = nota;
-            asignatura.creditos = creditos;
-            asignatura.nombre = nombre;
-            asignatura.tipologia = tipologia;
-            asignatura.prerrequisitos = prerrequisitos;
-            asignatura.estado = estado;
-            asignatura.afecta_promedio = afecta_promedio;
-        }
-    });
-    res.status(200).json(asignaturas["asignaturas"]);
-    } else{
-        res.status(500).json({error: 'There was an error.'});
-    }
-});
+    // Generate filename for the input JSON file
+    const inputFilename = "input.json";
 
-router.delete('/:semestre/:codigo', (req, res) => {
-    const semestre = parseInt(req.params.semestre);
-    const codigo = req.params.codigo;
-    const index = _.findIndex(asignaturas["asignaturas"], 
-                            function(asignatura) { return (asignatura.semestre === semestre && 
-                                                asignatura.codigo === codigo) 
-                        });
-    asignaturas["asignaturas"].splice(index, 1);
-    res.status(200).json(asignaturas["asignaturas"]);
-});
+    // Write the input JSON to a file
+    fs.writeFileSync(inputFilename, JSON.stringify(input));
 
+    // Generate filename for the output PDF file
+    const outputFilename = "output.pdf";
+
+    // Run the Python script with the input and output filenames as arguments
+    const pyprog = spawn('python', ['my_script.py', inputFilename, outputFilename]);
+
+    // Send the output PDF as a file download when the Python script finishes
+    pyprog.on('close', (code) => {
+    const outputPdf = fs.readFileSync(outputFilename);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename=${outputFilename}`);
+    res.send(outputPdf);
+
+    // Clean up the input and output files
+    fs.unlinkSync(inputFilename);
+    fs.unlinkSync(outputFilename);
+  });
+});
 
 
 module.exports = router;
